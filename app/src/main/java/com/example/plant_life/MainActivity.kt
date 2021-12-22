448b389068bd1bd4d1d80f20589adef9d656da0a
haya <haya.saud.ruh.k@tuwaiq.edu.sa>
2021-12-23 02:00:47 +0300

    SignUp - Login and show user information

--- app/src/main/java/com/example/plant_life/MainActivity.kt
+++ app/src/main/java/com/example/plant_life/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.plant_life
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.plant_life.databinding.ActivityLoginBinding
+import com.example.plant_life.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //ViewBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.start.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
